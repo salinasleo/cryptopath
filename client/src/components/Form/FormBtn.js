062f3e889455df34c9e0bf60ea0e1e7e1e49be1d
salinasleo <salinasleo@gmail.com>
2018-10-13 15:29:58 -0400

    buttons spaced

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
-    {props.children}
+  <button {...props} style={{ float: "left", marginBottom: 10, marginright: 10, marginleft:10, paddingleft: 10, paddingright: 10 }} className="btn btn-seondary">
+  {props.children} 
   </button>
+
 );
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Welcome from "../../components/Welcome";
 import DeleteBtn from "../../components/DeleteBtn";
 import API from "../../utils/API";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, TextArea, FormBtn, FormBtn2 } from "../../components/Form";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 
@@ -15,7 +15,7 @@ class Dashboard extends Component {
 
   // When the form is submitted, use the API.saveUser method to save the user data
   // Then reload users from the database
-  handleFormSubmit = event => {
+  signOn = event => {
     event.preventDefault();
     if (this.state.username && this.state.password) {
       console.log("you clicked button");
@@ -28,6 +28,39 @@ class Dashboard extends Component {
         .catch(err => console.log(err));
     }
   };
+
+  registerUser = event => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      console.log("you clicked button");
+
+      API.saveUser({
+        username: this.state.username,
+        password: this.state.password
+      })
+        .then(res => this.loadCoins())
+        .catch(err => console.log(err));
+    }
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (this.state.password.length < 6) {
+      alert(`Choose a more secure (longer) password`);
+      return;
+    } if (this.state.username && this.state.password) {
+      console.log("you clicked button");
+
+      API.saveUser({
+        username: this.state.username,
+        password: this.state.password
+      })
+        .then(res => this.loadCoins())
+        .catch(err => console.log(err));
+    }
+  };
+
 
 
 
@@ -54,42 +87,52 @@ class Dashboard extends Component {
           <Col size="md-4"></Col>
           <Col size="md-4">
             <h4>Login to see your portfolio</h4>
-            </Col>
-            </Row>
-          <Row>
-            <Col size="md-5"></Col>
-            <Col size="md-2">
-              <form>
-                <Input
-                  value={this.state.username}
-                  onChange={this.handleInputChange}
-                  name="username"
-                  placeholder="User Name: "
-                />
-                <Input
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                  name="password"
-                  placeholder="Password: "
-                  type="password"
-                />
-                <FormBtn
-                  disabled={!(this.state.username && this.state.password)}
-                  onClick={this.handleFormSubmit}
-                >
-                  Sign on
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-5"></Col>
+          <Col size="md-2">
+            <form>
+              <Input
+                value={this.state.username}
+                onChange={this.handleInputChange}
+                name="username"
+                placeholder="User Name: "
+              />
+              <Input
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                name="password"
+                placeholder="Password: "
+                type="password"
+              />
+
+              <FormBtn
+                disabled={!(this.state.username && this.state.password)}
+                onClick={this.signOn}
+              >
+                Sign on
               </FormBtn>
-              </form>
-            </Col>
-          </Row>
-          {/* <Row>
+
+              <FormBtn2
+                disabled={!(this.state.username && this.state.password)}
+                onClick={this.handleFormSubmit}
+              >
+                Register New
+              </FormBtn2>
+
+
+            </form>
+          </Col>
+        </Row>
+        {/* <Row>
           <Col size="md-12 sm-12">
             <h1>Coins in your portfolio</h1>
           </Col>
         </Row> */}
       </Welcome>
-        );
-      }
-    }
-    
-    export default Dashboard;
+    );
+  }
+}
+
+export default Dashboard;
