10f57e5e872bbe30c248ce57a9c0ec13cc8b6544
salinasleo <salinasleo@gmail.com>
2018-10-14 19:22:53 -0400

    new user registration2

---
+++ client/src/components/AddCoin/AddCoin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AddCoin.css";
+
+const AddCoin = () => (
+<div>
+    </div>
+);
+
+
+export default AddCoin;
--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -1,25 +1,87 @@
-import React from "react";
+import React , { Component } from "react";
 import "./Portfolio.css";
 import { Col, Row, Container } from "../../components/Grid";
+import AddCoin from "../../components/AddCoin";
 import $ from 'jquery';
+import { Input, TextArea, FormBtn, FormBtn2 } from "../../components/Form";
 
-const Portfolio = ({ children }) => (
-        <div className="jumbotron text-center">
-                <h7>Welcome {localStorage.authentificateduser}! &nbsp; &nbsp; <span title="Logout" class="fas fa-sign-out-alt" id="logout"></span> </h7>
-            <Row>
-                <Col size="md-12 sm-12">
-                    <h2>Coins in your portfolio</h2>
-                </Col>
-            </Row>
-            {children}
-        </div>
-);
 
 $(document).on("click", '#logout', logout);
 
 function logout() {
     localStorage.clear();
     window.location.reload();
+};
+
+const datenow=  Date.now();
+// const monthplaceholder = datenow.getMonth();
+// const yearplaceholder = datenow.getDay();
+// const dayplaceholder = datenow.getYear();
+
+class Portfolio extends Component {
+    state = {
+        addcoins: false
+    };
+
+    renderPage = () => {
+        if (this.state.addcoins) {
+            return <AddCoin />;
+        }
+    };
+
+    render() {
+        return (
+                <div className="jumbotron text-center">
+                    <h7>Welcome {localStorage.authentificateduser}! &nbsp; &nbsp; <span title="Logout" class="fas fa-sign-out-alt" id="logout"></span> </h7>
+                    <Row>
+                        <Col size="md-12 sm-12">
+                            <h2>Coins in your portfolio <span title="Add Coins" class="fa fa-plus" id="addcoin"></span></h2>
+                        </Col>
+                    </Row>
+
+
+<Row>
+<Col size="md-5"></Col>
+<Col size="md-2">
+  <form>
+    <Input
+      value={this.state.username}
+      onChange={this.handleInputChange}
+      name="Coin"
+      placeholder="BTC"
+    />
+    <Input
+      value={this.state.password}
+      onChange={this.handleInputChange}
+      name="Quantity"
+      placeholder="Quantity"
+    />
+    <Input
+      value={this.state.password}
+      onChange={this.handleInputChange}
+      name="Cost Basis"
+      placeholder="$USD Cost"
+    />
+    <Input
+      value={this.state.password}
+      onChange={this.handleInputChange}
+      name="Purchase Date"
+      placeholder= "10/16/2018" /*{datenow}*/
+    />
+    <FormBtn
+      disabled={!(this.state.username && this.state.password)}
+      onClick={this.signOn3}
+    >
+      Add Coin
+    </FormBtn>
+  </form>
+</Col>
+</Row>
+
+            </div>
+ );
+}
+
 };
 
 export default Portfolio;
